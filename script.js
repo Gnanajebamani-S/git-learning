3790f2346734340afff85c244e16ac1e8713fc6e
Gnanajebamani-S <jebamani435@gmail.com>
2024-02-05 12:46:46 +0530

    changes

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 var request =  new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all")
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
+request.send();
+request.onload = function(){
+    var res = JSON.parse(request.response);
+    console.log(res);
+    var result = res.filter((ele)=>ele.population<200000);
+   var capitalName = result.map((ele)=>ele.capital)
+   console.log(capitalName)
+}
